e515e26bbbcad6b0ff68f4ecb95adc38e13bf69d
Nick <n.t.1996@mail.ru>
2025-11-10 19:06:32 +0400

    chore(types): refine module declarations for assets and styles

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,15 +1,60 @@
-declare module '*.mp3';
-declare module '*.wav';
-declare module '*.ogg';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.png';
-declare module '*.gif';
-declare module '*.svg';
-declare module '*.webp';
-declare module '*.avif';
-declare module '*.ttf';
-declare module '*.otf';
-declare module '*.woff';
-declare module '*.woff2';
-declare module '*.scss';
+declare module '*.mp3' {
+	const src: string;
+	export default src;
+}
+declare module '*.wav' {
+	const src: string;
+	export default src;
+}
+declare module '*.ogg' {
+	const src: string;
+	export default src;
+}
+declare module '*.jpg' {
+	const src: string;
+	export default src;
+}
+declare module '*.jpeg' {
+	const src: string;
+	export default src;
+}
+declare module '*.png' {
+	const src: string;
+	export default src;
+}
+declare module '*.gif' {
+	const src: string;
+	export default src;
+}
+declare module '*.svg' {
+	const src: string;
+	export default src;
+}
+declare module '*.webp' {
+	const src: string;
+	export default src;
+}
+declare module '*.avif' {
+	const src: string;
+	export default src;
+}
+declare module '*.ttf' {
+	const src: string;
+	export default src;
+}
+declare module '*.otf' {
+	const src: string;
+	export default src;
+}
+declare module '*.woff' {
+	const src: string;
+	export default src;
+}
+declare module '*.woff2' {
+	const src: string;
+	export default src;
+}
+declare module '*.scss' {
+	const classes: Record<string, string> | string;
+	export default classes;
+}
